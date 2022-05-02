5852f3abf06f80c76c7ca7302843fd9f085846cf
Abhishek Parekh <abhishekparekh25@yahoo.ca>
2022-05-01 20:25:59 -0400

    Initial commit

---
+++ app/src/main/java/com/example/firstlook/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.firstlook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mButton;
+    private TextView mTextView;
+    private EditText mEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mButton = (Button) findViewById(R.id.mButton);
+        mTextView = (TextView) findViewById(R.id.mTextView);
+        mEditText = (EditText) findViewById(R.id.editText);
+
+        //change the title of the button
+        mButton.setText(R.string.button_name);
+
+    }
+
+    public void ShowMe(View view) {
+
+
+        mTextView.setVisibility(View.VISIBLE);
+        //mTextView.setText(R.string.show_text);
+
+        String enteredText;
+        enteredText = mEditText.getText().toString();
+        mTextView.setText(enteredText);
+    }
+}
